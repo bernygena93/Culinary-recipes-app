e148087588bf1a593a6981a4109a8ffd377270e2
bernygena93 <bernygena93@users.noreply.github.com>
2022-03-09 21:03:31 -0300

    culinary-recipes-app

--- culinaryRecipes/src/screens/recipes/RecipesScreen.js
+++ culinaryRecipes/src/screens/recipes/RecipesScreen.js
@@ -3,13 +3,20 @@ import React from 'react';
 // import recipes from '../../json/recipes.json';
 import Recipe from '../../components/card/Recipe';
 import {styles} from './styles';
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
 
 const RecipesScreen = ({navigation}) => {
   const recipes = useSelector(state => state.recipes.recipes);
+  const user = useSelector(state => state.user.user)
+  const dispatch = useDispatch();
   const navigateRecipesDetail = item => {
     navigation.navigate('recipesDetail', {recipe: item});
   };
+
+  useEffect(()=>{
+    dispatch(findAllRecipes(user.email))
+  },[])
+  
   return (
     <View style={styles.recipesContainer}>
       <FlatList
